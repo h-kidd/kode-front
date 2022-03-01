5549b1aae53884edcd44e3d97980627bfa0d6804
“jakcika” <jakcika@live.co.uk>
2022-03-01 17:45:37 +0000

    styles

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { HomeworkList, Title } from "../../components";
-import { makeStyles, Button, Container } from "@material-ui/core";
+import { makeStyles, Button, Container, Grid } from "@material-ui/core";
 import background from "../../img/background.jpg";
+import { dividerClasses } from "@mui/material";
 
 function Student() {
     const navigate = useNavigate()
@@ -21,23 +22,69 @@ function Student() {
             objectFit: "cover",
             height: "100vh",
         },
+        button: {
+            backgroundColor: "lightblue",
+            color: "white",
+            borderRadius: "10px",
+            marginTop: "10px",
+            borderColor: "lightblue",
+            width: "100px",
+            height: "40px"
+        },
+        container: {
+            backgroundColor: "white",
+            width: "400px",
+            height: "400px",
+            padding: "20px",
+            borderRadius: "30px",
+            position: "fixed",
+            marginTop: "270px",
+            marginLeft: "275px",
+            transform: "translate(-50%, -50%)",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "left",
+            display: "flex",
+            
+            },
+            item1: {
+
+
+            },
+            item2: {
+                border: "1px solid white"
+                }   
     })
     const classes = useStyles();
     return (
+        
         <div className={classes.background}>
             <Title />
 
-            <HomeworkList />
+        
+        <Grid container justify="flex-start">
 
-            <div >
-                <button id="joinGame" onClick={ joinGame }>
-                    Join Game
-                </button>
-                <button id="viewScores" onClick={ viewScores }>
-                    View Scores
-                </button>
-            </div>
+        <div className={classes.container}>
+        <div  className={classes.item1}>
+            <HomeworkList />
+        
         </div>
+        </div>
+        </Grid>
+    
+        <Grid container justify="flex-end">          
+            <Button variant="contained" className={classes.button} id="joinGame" onClick={ joinGame }>
+                    Join Game
+            </Button>      
+            < br/>      
+            <Button variant="contained" className={classes.button} id="viewScores" onClick={ viewScores }>
+                    View Scores
+            </Button>
+            
+            
+        
+    </Grid>
+    </div>
     )
 }
 
