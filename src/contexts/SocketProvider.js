43a1b68a7c0555ffc80d9dd201afaaef4ded0a94
Hayden Kidd <hkidd@live.co.uk>
2022-03-03 17:08:59 +0000

    socket link

--- src/contexts/SocketProvider.js
+++ src/contexts/SocketProvider.js
@@ -11,7 +11,7 @@ export function SocketProvider({ children }) {
   const [socket, setSocket] = useState()
 
   useEffect(() => {
-    const newSocket = io('http://localhost:5000')
+    const newSocket = io('https://kode-server.herokuapp.com/')
     setSocket(newSocket)
     return () => newSocket.close()
   }, [])
